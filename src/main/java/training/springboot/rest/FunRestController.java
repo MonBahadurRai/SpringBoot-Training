971dd5d48916e4d132420d531d86363267e2e1e1
Mon Bahadur Rai <monbahadurrai81@gmail.com>
2023-08-08 16:50:25 +0600

    added first rest controller

--- src/main/java/training/springboot/rest/FunRestController.java
+++ src/main/java/training/springboot/rest/FunRestController.java
@@ -1,2 +1,14 @@
-package training.springboot.rest;public class FunRestController {
+package training.springboot.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+//    expose "/" that return "HELLO WORLD"
+
+    @GetMapping(value = "")
+    private String sayHelloWorld() {
+         return "Hello World!";
+    }
 }
