994f292d7941054c91f83c66475fbfad01a9947a
Mon Bahadur Rai <monbahadurrai81@gmail.com>
2023-08-08 17:18:01 +0600

    reading the properties file value from application.properties

--- src/main/java/training/springboot/rest/FunRestController.java
+++ src/main/java/training/springboot/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package training.springboot.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class FunRestController {
 //    expose "/" that return "HELLO WORLD"
 
+
+    //Reading the application.properties file values
+    @Value("${project.name}")
+    private String projectName;
+    //  project.name=Spring Boot Training
+
+
     @GetMapping(value = "")
     private String sayHelloWorld() {
-         return "Hello World!";
+        System.out.println(this.projectName);
+        return "Hello World!";
     }
 }
