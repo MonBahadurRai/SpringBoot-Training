7b601551026a3c467651f1ca1fffc6c0afaba5f5
Mon Bahadur Rai <monbahadurrai81@gmail.com>
2023-08-09 15:45:30 +0600

    injecting the custom properties file

--- src/main/java/training/springboot/rest/FunRestController.java
+++ src/main/java/training/springboot/rest/FunRestController.java
@@ -1,10 +1,13 @@
 package training.springboot.rest;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
+@PropertySource("classpath:domain.properties")
 public class FunRestController {
 
 
@@ -13,13 +16,27 @@ public class FunRestController {
     private String projectName;
     //  project.name=Spring Boot Training
 
+    //    Injecting the application.properties custom properties
+    @Value("${learning.mode}")
+    private String learningMode;
+
+
+    //    From Custom Properties
+    @Value("${route.url}")
+    private String routeUrl;
+
+    @Value("${route.code}")
+    private int routeCode;
+
 
     //Expose "/" that return "HELLO WORLD"
     @GetMapping(value = "")
     private String sayHelloWorld() {
         System.out.println(this.projectName);
-        return "Hello World!";
+        System.out.println(this.learningMode);
+        return this.projectName + " : " + this.learningMode + " == > " + this.routeUrl +" at "+ this.routeCode;
     }
+
     @GetMapping(value = "mon")
     private String sayMon() {
         System.out.println(this.projectName);
